8c44a55c4ea2fd61b540edec1bf917652e06fa1c
Bhavik Kumar <bhavikk85@gmail.com>
2017-07-14 20:05:18 +1200

    Add ability to assume roles as optional parameters

--- aws/windows/create-temporary-profile.ps1
+++ aws/windows/create-temporary-profile.ps1
@@ -6,22 +6,52 @@
 
 param (
    [Parameter(Mandatory=$true)][string]$profile,
-   [Parameter(Mandatory=$true)][string]$mfaArn,
+   [Parameter(Mandatory=$true)][string]$identityAccount,
+   [Parameter(Mandatory=$true)][string]$username,
+   [Parameter(Mandatory=$false)][string]$assumeRoleAccount,
+   [Parameter(Mandatory=$false)][string]$roleName,
    [Parameter(Mandatory=$true)][string]$mfatoken
 )
 
 # This script could be replaced with AWS Tools for Windows Powershell
-$Response = (aws sts get-session-token --serial-number $mfaArn --token-code $mfatoken --profile $profile)  | ConvertFrom-Json
+$mfaAccount = "arn:aws:iam::" + $identityAccount + ":mfa/" + $username
+$Response = (aws sts get-session-token --serial-number $mfaAccount --token-code $mfatoken --profile $profile)  | ConvertFrom-Json
 
-$NewAccessKeyId = $Response.Credentials.AccessKeyId
-$NewSecretKey = $Response.Credentials.SecretAccessKey
-$NewSessionToken =  $Response.Credentials.SessionToken
-$ExpirationTime = $Response.Credentials.Expiration
-$TempProfile = $profile + "-temp"
+If(![string]::IsNullOrEmpty($Response)) {
+  $NewAccessKeyId = $Response.Credentials.AccessKeyId
+  $NewSecretKey = $Response.Credentials.SecretAccessKey
+  $NewSessionToken =  $Response.Credentials.SessionToken
+  $ExpirationTime = $Response.Credentials.Expiration
+  $MFAProfile = $profile + "-mfa"
 
-aws configure set aws_access_key_id $NewAccessKeyId --profile $TempProfile
-aws configure set aws_secret_access_key $NewSecretKey --profile $TempProfile
-aws configure set aws_session_token $NewSessionToken --profile $TempProfile
+  aws configure set aws_access_key_id $NewAccessKeyId --profile $MFAProfile
+  aws configure set aws_secret_access_key $NewSecretKey --profile $MFAProfile
+  aws configure set aws_session_token $NewSessionToken --profile $MFAProfile
 
-Write-Host "Profile Name: $TempProfile"
-Write-Host "Expires At : $ExpirationTime"
+  Write-Host "Created MFA Profile"
+  Write-Host "Profile Name: $MFAProfile"
+  Write-Host "Expires At : $ExpirationTime"
+}
+
+If (![string]::IsNullOrEmpty($assumeRoleAccount) -and ![string]::IsNullOrEmpty($roleName)) {
+  $RoleProfileName = $username + "-" + $roleName
+  $roleAccount = "arn:aws:iam::" + $assumeRoleAccount + ":role/" + $roleName
+
+  $AssumeRoleResponse = (aws sts assume-role --role-arn $roleAccount --role-session-name $RoleProfileName --profile $MFAProfile) | ConvertFrom-Json
+
+  If (![string]::IsNullOrEmpty($AssumeRoleResponse)) {
+    $AssumeRoleAccessKey = $AssumeRoleResponse.Credentials.AccessKeyId
+    $AssumeRoleSecretKey = $AssumeRoleResponse.Credentials.SecretAccessKey
+    $AssumeRoleSessionToken =  $AssumeRoleResponse.Credentials.SessionToken
+    $AssumeRoleExpirationTime = $AssumeRoleResponse.Credentials.Expiration
+
+    aws configure set aws_access_key_id $AssumeRoleAccessKey --profile $RoleProfileName
+    aws configure set aws_secret_access_key $AssumeRoleSecretKey --profile $RoleProfileName
+    aws configure set aws_session_token $AssumeRoleSessionToken --profile $RoleProfileName
+
+    Write-Host "------"
+    Write-Host "Created Assumed Role Profile"
+    Write-Host "Profile Name: $RoleProfileName"
+    Write-Host "Expires At : $AssumeRoleExpirationTime"
+  }
+}
